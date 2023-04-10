09a2c0a62f408fe8d4972c66d35143d8998302e5
luka_CB <lukaaslamazashvili20@gmail.com>
2023-04-10 19:24:42 +0400

    test2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ import {
 import SearchResult from "../components/search/SearchResult";
 import MobileNavigationHome from "../components/navigation/MobileNavigationHome";
 import Head from "../components/Head";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [windowWidth, setWindowWidth] = useState("");
@@ -27,6 +28,7 @@ const Home = () => {
   );
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getLatestAccessories());
@@ -72,6 +74,12 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <button onClick={() => navigate("/test")}>
+        <h1>Test</h1>
+      </button>
+      <button onClick={() => navigate("/redirect")}>
+        <h1>Redirect</h1>
+      </button>
 
       <SlideShow />
 
