30ed3146accebd6eaf33bc44eb0ff6bb34cc2766
luka_CB <lukaaslamazashvili20@gmail.com>
2023-04-12 12:50:35 +0400

    fixed some issues

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://techstore-server-production.up.railway.app",
   headers: { "Content-Type": "application/json" },
 });
 
